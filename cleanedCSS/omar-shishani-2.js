1d8b2b90f9e33a4797b8f7f06ad76b8825ac2a28
Omar Shishani <omarshishanischool@gmail.com>
2020-09-30 16:33:09 -0700

    Fix more SMACSS, add header--heading to layout

--- cleanedCSS/omar-shishani-2.js
+++ cleanedCSS/omar-shishani-2.js
@@ -1,12 +1,12 @@
 let fadeCompleted = false;
 function runslide_O() {
-  $('.o-plain').delay(1500).fadeOut(1500).fadeIn(1500, function(){
+  $('.os-o-plain').delay(1500).fadeOut(1500).fadeIn(1500, function(){
   	fadeCompleted = true; 
   });
 };
 function runslide_S() {
 	fadeCompleted = false;
-  $('.s-plain').fadeOut(1500).fadeIn(1500);
+  $('.os-s-plain').fadeOut(1500).fadeIn(1500);
 };
 function runslide_both() {
 	if (fadeCompleted === true) {
@@ -22,10 +22,10 @@ $('.os-v2-svg').on('click', runslide_both);
 
 document.onload = runslide_O_load(), runslide_S_load();
 function runslide_O_load() {
-  $('.o-plain').fadeIn(2000);
+  $('.os-o-plain').fadeIn(2000);
 };
 function runslide_S_load() {
-  $('.s-plain').delay(1500).fadeIn(2000, function(){
+  $('.os-s-plain').delay(1500).fadeIn(2000, function(){
   	fadeCompleted = true;
   });
 };
